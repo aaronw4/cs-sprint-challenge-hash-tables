3735f99113cd56ceed8ed07600afeac759da5c41
Aaron Williams <aaronrw4@yahoo.com>
2020-09-11 20:52:47 -0700

    Finished ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,13 +1,22 @@
-# Your code here
-
-
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    dic = {}
 
+    for i in range(0, len(files)):
+        split_file = files[i].split('/')
+        if split_file[-1] not in dic.keys():
+            dic[split_file[-1]] = [files[i]]
+        else:
+            dic[split_file[-1]].append(files[i])
+    
+    result = []
+    
+    for i in range(0, len(queries)):
+        if queries[i] not in dic.keys():
+            pass
+        else:
+            for index in range(0, len(dic[queries[i]])):
+                result.append(dic[queries[i]][index])
+    
     return result
 
 
